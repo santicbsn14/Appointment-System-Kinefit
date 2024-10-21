0553d356b50a703addcb721d674d44a447bd6f22
santiago_viale <santicbsn9@gmail.com>
2024-10-21 10:23:41 -0300

    configuracion de cambio de password.2

--- Source/Domain/Manager/userManager.ts
+++ Source/Domain/Manager/userManager.ts
@@ -7,7 +7,6 @@ import createUserValidation from "../Validations/CreatesValidation/createUserVal
 import updateUserValidation from "../Validations/UserValidations/updateUserValidation";
 import { validPassword } from "../../Utils/hashService";
 import admin from "firebase-admin";
-import { createHash } from "crypto";
 
 class UserManager {
     private userRepository
@@ -32,13 +31,14 @@ class UserManager {
     }
     async updateUser(body:IUser, id:IdMongo){
         await updateUserValidation.parseAsync({...body, id})
-        let {password, ...bodyUserDto} = body
-        const hashedPassword = await createHash(password)
-        let userWithPassword = {...bodyUserDto,
-            password:hashedPassword
-        }
+        let user: IUser = await this.userRepository.getUserByEmail(body.email)
+        const isHashedPassword = validPassword(body.password, user.password)
+        if (!isHashedPassword)
+            {
+                throw new Error('Updated failed, invalid password.');
+            }
         
-        return await this.userRepository.updateUser(id, userWithPassword)
+        return await this.userRepository.updateUser(id, body)
     }
     async deleteUser(id: IdMongo){
         await idValidation.parseAsync(id)
